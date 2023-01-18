923b98f8eba63b766cc5a19da9a673c9c4cd8e0d
denys <denamyte@gmail.com>
2023-01-18 02:11:58 +0200

    2023-01-18: Stage 1/5: finished

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,1 +1,15 @@
-# write your code here
+import socket
+from sys import argv
+
+
+def main():
+    ip, port, message = argv[1], int(argv[2]), argv[3]
+    with socket.socket() as s:
+        s.connect((ip, port))
+        s.send(message.encode())
+        b_resp = s.recv(1024)
+        print(b_resp.decode())
+
+
+if __name__ == '__main__':
+    main()
---
+++ Password Hacker/task/hacking/host.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 9090
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if data and data.decode('utf-8') != 'qwerty':
+                conn.send(b"Wrong password!")
+            elif data:
+                conn.send(b"Connection Success!")
+            elif not data:
+                break
